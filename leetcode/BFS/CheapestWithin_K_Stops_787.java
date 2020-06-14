dc2e2a463e122dd53c3851b6b07324d13b3b8f4c
r3mtm <25997802+r3mtm@users.noreply.github.com>
2020-06-14 17:02:01 +0530

    new problem in "BFS"

---
+++ leetcode/BFS/CheapestWithin_K_Stops_787.java
@@ -0,0 +1,116 @@
+/*
+
+TAGS={BFS, Dynamic Programming}
+
+787. Cheapest Flights Within K Stops
+
+There are n cities connected by m flights. Each flight starts from city u and arrives at v with a price w.
+
+Now given all the cities and flights, together with starting city src and the destination dst, your task is to find the cheapest price from src to dst with up to k stops. If there is no such route, output -1.
+
+Example 1:
+Input: 
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marked red in the picture.
+
+
+Example 2:
+Input: 
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 0
+Output: 500
+Explanation: 
+The cheapest price from city 0 to city 2 with at most 0 stop costs 500, as marked blue in the picture.
+ 
+
+Constraints:
+
+The number of nodes n will be in range [1, 100], with nodes labeled from 0 to n - 1.
+The size of flights will be in range [0, n * (n - 1) / 2].
+The format of each flight will be (src, dst, price).
+The price of each flight will be in the range [1, 10000].
+k is in the range of [0, n - 1].
+There will not be any duplicated flights or self cycles.
+
+*/
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Queue;
+
+class Solution {
+    
+    class Data {
+        
+        int city;
+        int cost;
+        int stops;
+        
+        public Data(int city, int cost, int stops) {
+            this.city = city;
+            this.cost = cost;
+            this.stops = stops;
+        }
+        
+    }
+    
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        int min_cost = Integer.MAX_VALUE;
+        
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        Queue<Data> queue = new LinkedList<>();
+        
+        for (int i = 0; i < flights.length; ++i) {
+            graph.putIfAbsent(flights[i][0], new ArrayList<>());
+            graph.get(flights[i][0]).add(new int[]{flights[i][1], flights[i][2]});
+        }
+        
+        queue.add(new Data(src, 0, 0));
+        
+        while (queue.size() > 0) {
+            Data currentCity = queue.poll();
+            
+            if (currentCity.city == dst) {
+                if (currentCity.stops <= K + 1) { // K + 1 since, last stop may be the target city/stop
+                    if (currentCity.cost < min_cost) {
+                        min_cost = currentCity.cost;
+                    }
+                } else {
+                    continue;
+                }
+            } else if (currentCity.stops > K) {
+                continue;
+            } 
+            
+            if (graph.containsKey(currentCity.city)) {
+                List<int[]> list = graph.get(currentCity.city);
+                for (int[] item : list) {
+                    if (item[1] + currentCity.cost <= min_cost) { //
+                        queue.add(new Data(item[0], (item[1] + currentCity.cost), (currentCity.stops + 1)));
+                    }
+                }
+            }
+            
+        }
+        
+        return min_cost == Integer.MAX_VALUE ? -1 : min_cost;
+    }
+}
+
+public class CheapestWithin_K_Stops_787 {
+    public static void main (String[] args) {
+        int n = 3, src = 0, dst = 2, K = 1;
+        int[][] flights = {
+                            {0,1,100},
+                            {1,2,100},
+                            {0,2,500}
+                        };
+        System.out.println(new Solution().findCheapestPrice(n, flights, src, dst, K));
+    }
+}
