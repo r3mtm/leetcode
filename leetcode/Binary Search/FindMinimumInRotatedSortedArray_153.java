82c668f6d17334a3b20996582451543cad1d07d0
Remil Michael <25997802+r3mtm@users.noreply.github.com>
2020-07-25 16:43:07 +0530

    new problem in "Binary Search"

---
+++ leetcode/Binary Search/FindMinimumInRotatedSortedArray_153.java
@@ -0,0 +1,57 @@
+/*
+
+TAGS={Binary Search, Array}
+
+153. Find Minimum in Rotated Sorted Array
+
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+
+Example 1:
+
+Input: [3,4,5,1,2] 
+Output: 1
+
+
+Example 2:
+
+Input: [4,5,6,7,0,1,2]
+Output: 0
+
+*/
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1, mid = 0;
+        int min = nums[0];
+        
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            min = Math.min(min, nums[mid]);
+            
+            if (nums[left] > nums[right]) {
+                if (nums[mid] > nums[left] || nums[mid] > nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                right = mid - 1;
+            }
+        }
+        return min;
+    }
+}
+
+public class FindMinimumInRotatedSortedArray_153 {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println(new Solution().findMin(nums));
+    }
+}
