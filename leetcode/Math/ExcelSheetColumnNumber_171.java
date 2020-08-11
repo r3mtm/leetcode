53495265ea857ac537c423356262c9a88fe20b02
Remil Michael <25997802+r3mtm@users.noreply.github.com>
2020-08-11 08:43:12 +0530

    new problem in "Math"

---
+++ leetcode/Math/ExcelSheetColumnNumber_171.java
@@ -0,0 +1,63 @@
+/*
+
+TAGS={Math}
+
+171. Excel Sheet Column Number
+
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+
+Example 1:
+
+Input: "A"
+Output: 1
+
+Example 2:
+
+Input: "AB"
+Output: 28
+
+Example 3:
+
+Input: "ZY"
+Output: 701
+
+Constraints:
+
+1 <= s.length <= 7
+s consists only of uppercase English letters.
+s is between "A" and "FXSHRXW".
+
+
+*/
+
+class Solution {
+    public int titleToNumber(String s) {
+        int base = 1, total = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            int val = s.charAt(i) - 'A' + 1;
+            total += base * val;
+            base *= 26;
+        }
+        
+        return total;
+    }
+}
+
+public class ExcelSheetColumnNumber_171 {
+    public static void main(String[] args) {
+        String s = "ZZ";
+        System.out.println(new Solution().titleToNumber(s));
+    }
+}
